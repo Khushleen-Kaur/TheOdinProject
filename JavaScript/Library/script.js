12f005db8b8dd4761b006d507fb21a55753f4798
Khushleen Kaur <khushleenkaur202007@gmail.com>
2026-07-30 23:16:01 +0530

    2nd Commit - Basic frontend

--- JavaScript/Library/script.js
+++ JavaScript/Library/script.js
@@ -1,1 +1,16 @@
 console.log("Welcome to Khushleen's Library!");
+
+const myLibrary = [];
+
+class Book{
+    Book() {
+        id = crypto.randomUUID();
+
+    }
+}
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+
+}
+
+
