270d20926aa424782ece1ef805b3969935897d33
Khushleen Kaur <khushleenkaur202007@gmail.com>
2026-07-31 10:13:54 +0530

    Add Dialog box

--- JavaScript/Library/script.js
+++ JavaScript/Library/script.js
@@ -3,14 +3,33 @@ console.log("Welcome to Khushleen's Library!");
 const myLibrary = [];
 
 class Book{
-    Book() {
-        id = crypto.randomUUID();
-
+    Book(title, author, pages , coverUrl) {
+        this.id = crypto.randomUUID();
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.coverUrl = coverUrl;
+        console.log(this.title);
     }
 }
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
-
+function addBookToLibrary(book) {
+    
 }
 
+let form = document.querySelector("#myForm");
+form.addEventListener("submit", (() => {
+    event.preventDefault();
+    
+    let t = document.getElementById("title").value;
+    let a = document.getElementById("author").value;
+    let p = document.getElementById("pages").value;
+    let u = document.getElementById("coverUrl").value;
+    console.log(t,a,p,u);
+    let book = new Book(t,a,p,u);
+
+    myLibrary.push(book);
+    console.log(myLibrary);
+    alert("Book Added");
+    document.querySelector("#my-dialog").close();
+}));
 
